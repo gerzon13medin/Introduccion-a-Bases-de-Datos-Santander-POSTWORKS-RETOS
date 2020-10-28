121b2830315ccfdbd2627d2e4d2648f42d83ce08
gerzon13medin <51671458+gerzon13medin@users.noreply.github.com>
2020-10-28 12:46:27 -0600

    Add files via upload

---
+++ Reto 1.sql
@@ -0,0 +1,9 @@
+/* Usando la base de datos tienda, muestra la descripción de las tablas articulo, puesto y venta. 
+Por cada tipo de dato que encuentras llena la siguiente tabla, a mano. Usa la Documentación de MySQL 
+como referencia. */
+
+SHOW DATABASES;
+DESCRIBE articulo;
+DESCRIBE puesto;
+DESCRIBE venta;
+
