d072b65625591e6bcb1c2c700d70eafa0bf32968
gerzon13medin <51671458+gerzon13medin@users.noreply.github.com>
2020-10-28 13:01:51 -0600

    Update Reto 2.sql

--- Reto 2.sql
+++ Reto 2.sql
@@ -9,6 +9,7 @@ SELECT id_puesto, salario FROM puesto WHERE salario > 10000;
 SELECT nombre,precio, iva FROM articulo WHERE precio > 1000 AND iva > 100;
 
 # ¿Qué ventas incluyen los artículo 135 o 963 y fueron hechas por los empleados 835 o 369?
+SELECT * FROM venta WHERE id_articulo IN(135,963) AND id_empleado IN(835,369);
 
 
 
